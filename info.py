e6c66975ef2d0153d18fb89a8c76f74f0eab3f16
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2025-02-01 23:28:06 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,7 @@
+from os import environ
+
+API = environ.get("API", "75a4e57fd825a0d5f9c5d04f9638712b31d08cdc") # shortlink api
+URL = environ.get("URL", "instantearn.in") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+Gt55OVP7VTAyNmNl") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "Ggjkkkooo_bot") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
