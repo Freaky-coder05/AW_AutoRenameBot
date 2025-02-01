a386bafdf1f6011b2550ccb7edd58f8f1592ca43
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2025-02-01 23:54:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -57,7 +57,7 @@ async def check_verification(bot, userid):
     tz = pytz.timezone('Asia/Kolkata')
     now = datetime.now(tz)  # Get current time in IST
 
-    if user.id in Config.ADMIN:
+    if user.id in ADMIN:
         return True  # Admins are always verified
 
     if user.id in VERIFIED:
